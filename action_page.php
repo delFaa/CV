3dfc4b5ac831aebbb7c1fae154ba1bc025fd7def
Elisa della Faille <elisa.della.faille@gmail.com>
2017-10-12 16:58:48 +0200

    php fonctionne

--- action_page.php
+++ action_page.php
@@ -1,9 +1,46 @@
 <?php include "include/header.php" ?>
 
-<div class="container">
-Thank you for your message,  <?php echo $_POST["yourname"]; ?><br>
-You will be recontacted in the shortest delays.
-</div>
+<?php
+/* Email du destinataire (mettez ici votre email) */
+$destinataire = "elisa.della.faille@gmail.com";
 
+/* RÃcupÃration */
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$header = "From: " . $_POST['email'];
+
+/* VÃrification */
+if ( $nom == "" ) {
+echo "Veuillez indiquer le nom.";
+$erreur = true;
+}
+if ( $subject == "" ) {
+echo "Veuillez indiquer le sujet.";
+$erreur = true;
+}
+if ( $message == "" ) {
+echo "Veuillez indiquer un message.";
+$erreur = true;
+}
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+echo "E-mail is not valid";
+$erreur = true;
+}
+
+/* Envoi du mail */
+if ( !$erreur ) {
+mail( $destinataire , $objet , $message , $header );
+} else {
+echo "erreur";
+}
+
+Thank you for your message,  <?php echo $_POST["yourname"]; ?><br>
+You will be contacted in the shortest delays.
+
+
+?>
 
 <?php include "include/footer.php" ?>
